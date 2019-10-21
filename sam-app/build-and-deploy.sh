70cad917943494ba917135396e9f8ba777e4480b
Ruan Beukes <ruan.coder@gmail.com>
2019-10-21 13:28:01 +0800

    deploy via SAM Cli and not aws cli

--- sam-app/build-and-deploy.sh
+++ sam-app/build-and-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sam build && 
 sam package --profile Ruan --region ap-southeast-2 --template-file template.yaml --output-template-file packaged.yaml --s3-bucket aws-workshop-ruan &&
-aws cloudformation deploy --profile Ruan --region ap-southeast-2 --template-file packaged.yaml --stack-name AWSWorkshopRuan --capabilities CAPABILITY_IAM
+sam deploy --profile Ruan --region ap-southeast-2 --template-file ./packaged.yaml --stack-name AWSWorkshopRuan --capabilities CAPABILITY_IAM &&
+aws cloudformation --profile Ruan --region ap-southeast-2 describe-stacks --stack-name AWSWorkshopRuan --query 'Stacks[]'
