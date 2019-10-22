fd57ce583639a8332907d197da4bfbb62077bcbb
Ruan Beukes <ruan.coder@gmail.com>
2019-10-22 13:55:38 +0800

    update deploy shell script

--- sam-app/build-and-deploy.sh
+++ sam-app/build-and-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sam build && 
+cd .aws-sam/build &&
 sam package --profile Ruan --region ap-southeast-2 --template-file template.yaml --output-template-file packaged.yaml --s3-bucket aws-workshop-ruan &&
 sam deploy --profile Ruan --region ap-southeast-2 --template-file ./packaged.yaml --stack-name AWSWorkshopRuan --capabilities CAPABILITY_IAM &&
-aws cloudformation --profile Ruan --region ap-southeast-2 describe-stacks --stack-name AWSWorkshopRuan --query 'Stacks[]'
+aws cloudformation --profile Ruan --region ap-southeast-2 describe-stacks --stack-name AWSWorkshopRuan --query 'Stacks[]' &&
+cd ../..
